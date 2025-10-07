3f093141614aa4a05c8febd3046cc3eb22ffa1f2
felipeoli7eira <me.felipeoliveira@gmail.com>
2025-10-06 23:30:50 -0300

    adiciona seed de um usuario padrao

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,11 @@ namespace Database\Seeders;
 
 // use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Domain\Entity\Usuario\Perfil;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +23,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('usuarios')->insert([
+            'uuid'   => \Illuminate\Support\Str::uuid(),
+            'nome'   => 'SOAT',
+            'email'  => 'soat@example.com',
+            'senha'  => Hash::make('padrao'),
+            'ativo'  => true,
+            'perfil' => Perfil::MECANICO->value,
+        ]);
     }
 }
