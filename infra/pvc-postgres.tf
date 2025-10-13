48644fcfa19e01fb1aa7d95aa2d7e02419b0371f
WILLIAM FRANCISCO LEITE <william@wllsistemas.com.br>
2025-10-13 19:27:44 -0300

    feat: adiciona camada de infra, k8s e readme

---
+++ infra/pvc-postgres.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_persistent_volume_claim" "lab_postgres_pvc" {
+  metadata {
+    name      = "lab-postgres-pvc"
+    namespace = kubernetes_namespace.lab_soat_terraform.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+    storage_class_name = ""
+  }
+}
