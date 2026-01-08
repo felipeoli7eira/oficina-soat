d0bbb247ede7b1c81e7003b67529b50b9b270d35
Felipe Oliveira <me.felipeoliveira@gmail.com>
2026-01-07 22:09:15 -0300

    feat: adiciona middleware de CPF na rota read-one de cliente

--- backend/routes/cliente.php
+++ backend/routes/cliente.php
@@ -2,11 +2,12 @@
 
 use App\Http\ClienteApi;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\DocumentoObrigatorioMiddleware;
 
 Route::post('/cliente', [ClienteApi::class, 'create']);
 
 Route::get('/cliente', [ClienteApi::class, 'read']);
-Route::get('/cliente/{uuid}', [ClienteApi::class, 'readOne']);
+Route::get('/cliente/{uuid}', [ClienteApi::class, 'readOne'])->middleware(DocumentoObrigatorioMiddleware::class);
 
 Route::put('/cliente/{uuid}', [ClienteApi::class, 'update']);
 Route::delete('/cliente/{uuid}', [ClienteApi::class, 'delete']);
